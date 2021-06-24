d86f71f915a91b11cb601c4ab4fa13ce401d09b6
kamorimpol <kamorim@yandex.by>
2021-06-24 18:11:58 +0300

    levels

--- Assets/Script/SlicedObjects/SlicedObjectsGroup.cs
+++ Assets/Script/SlicedObjects/SlicedObjectsGroup.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +7,10 @@ public class SlicedObjectsGroup : MonoBehaviour
     [SerializeField] float minimalVelocity = 0.8f;
     [SerializeField] float step = 0.1f;
     [SerializeField] int count = 50;
+    [SerializeField] Vector3 rotate;// = new Vector3 (0f, 0f, 0f);
     [SerializeField] Vector3 axis = Vector3.forward;
     [SerializeField] SlicedObject cardPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,7 @@ public class SlicedObjectsGroup : MonoBehaviour
         {
             Vector3 pos = transform.position + axis * step * i;
             SlicedObject slicedObject = Instantiate(cardPrefab, pos, Quaternion.identity, transform);
+            slicedObject.transform.eulerAngles += rotate;
             float slicePower = 1.0f - ((float)i / transform.childCount);
             slicePower = Mathf.Clamp(slicePower, minimalVelocity, 1.0f);
             slicedObject.SliceVelocityPower = slicePower;
@@ -35,5 +38,14 @@ public class SlicedObjectsGroup : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        float axisSize = step*count;
+        Vector3 size = axis * step*count + new Vector3 (3f, 1f, 0f);
+        Vector3 delta = axis * step*count*0.5f;
+        Gizmos.DrawWireCube(transform.position + delta, size);
     }
 }
