dd572d193ec1569fcdaaf365182bff559c270b3b
kamorimpol <kamorim@yandex.by>
2021-06-24 10:37:51 +0300

    Levels

---
+++ Assets/Script/FallingPlate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlate : MonoBehaviour
+{
+    private Rigidbody _rb;
+        // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        _rb.isKinematic = false;
+        Destroy(gameObject, 4f);
+    }
+}
---
+++ Assets/Script/MovingPlate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlate : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movement;
+    [SerializeField] private float _speed = 5;
+        
+    private Vector3 _targetPosition;
+    private bool _move = false;
+
+    private void Start() 
+    {
+        _targetPosition = gameObject.transform.position + _movement;
+    }
+    private void FixedUpdate() 
+    {
+        if (_move && transform.position != _targetPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.fixedDeltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        _move = true;
+    }
+}
